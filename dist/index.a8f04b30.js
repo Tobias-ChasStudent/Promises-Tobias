97b9541b8dc3750e8bfcbefcd83df86e7f7075ec
Tobias-ChasStudent <tobias.riiser@chasacademy.se>
2023-01-24 15:51:50 +0100

    completed chuck norris api + set up geolocation

--- dist/index.a8f04b30.js
+++ dist/index.a8f04b30.js
@@ -563,6 +563,9 @@ var _axiosDefault = parcelHelpers.interopDefault(_axios);
 const chuckURL = "https://api.chucknorris.io/jokes/random";
 const nasaURL = "https://api.nasa.gov/planetary/apod?api_key=0J9psijfNVWO8gC7iRadAgjWjCqmd9HwZw7WmJgb";
 const ranURL = "https://random.imagecdn.app/v1/image?&format=json";
+navigator.geolocation.getCurrentPosition((response)=>{
+    console.log("https://api.openweathermap.org/data/2.5/weather?&appid=401395c8bb11a6dde982bcf35f6812e4&units=metric&lat=" + response.coords.latitude + "&lon=" + response.coords.longitude);
+});
 async function getNasa() {
     await (0, _axiosDefault.default)(nasaURL).then((response)=>{
         try {
@@ -577,7 +580,38 @@ async function getNasa() {
         }
     });
 }
+async function getChuck() {
+    await (0, _axiosDefault.default)(chuckURL).then((response)=>{
+        try {
+            const chuck = response.data;
+            console.log(chuck.value);
+            document.querySelector(".chuck-norris").textContent = chuck.value;
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(async function(position) {
+        try {
+            await (0, _axiosDefault.default)("https://api.openweathermap.org/data/2.5/weather?&appid=401395c8bb11a6dde982bcf35f6812e4&units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude)//`api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=401395c8bb11a6dde982bcf35f6812e4`
+            .then((response)=>{
+                const weather = response.data;
+                const main = weather.main;
+                const wind = weather.wind;
+                console.log(main);
+                console.log(wind);
+                let iconURL = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`;
+                console.log(iconURL);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    });
+}
 getNasa();
+getChuck();
+getWeather();
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const nasaURL = "https://api.nasa.gov/planetary/apod?api_key=0J9psijfNVWO8gC7iRa
 
 const ranURL = "https://random.imagecdn.app/v1/image?&format=json";
 
+navigator.geolocation.getCurrentPosition((response) => {
+    console.log("https://api.openweathermap.org/data/2.5/weather?&appid=401395c8bb11a6dde982bcf35f6812e4&units=metric&lat=" + response.coords.latitude + "&lon=" + response.coords.longitude);
+})
 
 async function getNasa() {
     await axios(nasaURL)
@@ -24,5 +27,50 @@ async function getNasa() {
         )
 }
 
+async function getChuck() {
+    await axios(chuckURL)
+        .then(response => {
+            try {
+                const chuck = response.data
+                console.log(chuck.value);
+
+                document.querySelector(".chuck-norris").textContent = chuck.value
+
+            } catch (error) {
+                console.error(error);
+            }
+        })
+}
+function getWeather() {
+
+    navigator.geolocation.getCurrentPosition(async function (position){
+        try {
+            await axios("https://api.openweathermap.org/data/2.5/weather?&appid=401395c8bb11a6dde982bcf35f6812e4&units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude)
+
+            //`api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=401395c8bb11a6dde982bcf35f6812e4`
+
+
+                .then(response => {
+                    const weather = response.data;
+
+                    const main = weather.main;
+                    const wind = weather.wind;
+
+                    console.log(main);
+                    console.log(wind);
+
+                    let iconURL = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
+
+                    console.log(iconURL);
+
+                })
+
+        } catch (error) {
+            console.error(error);
+        }
+    })
+}
 
 getNasa()
+getChuck()
+getWeather()
